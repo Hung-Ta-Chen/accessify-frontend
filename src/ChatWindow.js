9eb8a7679f3237e2440bd9ebac0b0e481925944a
Hung-Ta Chen <htcchen@ucdavis.edu>
2024-05-31 22:34:43 -0700

    Update chat window for interacting with chatbot

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -1,7 +1,11 @@
-import { React, useState } from "react";
+import { React, useState, useEffect, useContext } from "react";
 import "./ChatWindow.css";
+import MapContext from "./MapContext";
+
+const SERVER_URL = process.env.REACT_APP_BACKEND_URL;
 
 function ChatWindow({ handleClose, isActive }) {
+  const { userLocation } = useContext(MapContext);
   const [messages, setMessages] = useState([
     {
       id: 0,
@@ -11,22 +15,76 @@ function ChatWindow({ handleClose, isActive }) {
     },
   ]);
   const [newMessage, setNewMessage] = useState("");
+  const [sessionID, setSessionID] = useState("");
+  const [conversationID, setConversationID] = useState("");
+
+  const startConversation = async (session_id) => {
+    const response = await fetch(`${SERVER_URL}/chatbot/conversation/start`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ session_id }),
+    });
+    const data = await response.json();
+    setConversationID(data.conversation_id);
+  };
+
+  useEffect(() => {
+    const startSession = async () => {
+      const response = await fetch(`${SERVER_URL}/chatbot/session/start`, {
+        method: "POST",
+      });
+      const data = await response.json();
+      setSessionID(data.session_id);
+      startConversation(data.session_id);
+    };
+    startSession();
+  }, []);
 
   // Function for sending a new message
-  const sendMessage = (event) => {
-    if (newMessage != "") {
-      event.preventDefault(); // Prevent the reload of the page
-      setMessages([
-        ...messages,
-        {
-          id: messages.length,
-          sender: "user",
-          text: newMessage,
-          timestamp: new Date(),
-        },
-      ]);
+  const sendMessage = async (event) => {
+    event.preventDefault(); // Prevent the reload of the page
+    if (newMessage !== "") {
+      const newMessageObj = {
+        id: messages.length,
+        sender: "user",
+        text: newMessage,
+        timestamp: new Date(),
+      };
+      setMessages([...messages, newMessageObj]);
+      setNewMessage(""); // Clear input box
+
       /* The part for sending the message list to the backend */
-      setNewMessage("");
+      try {
+        const response = await fetch(`${SERVER_URL}/chatbot/message/send`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            session_id: sessionID,
+            conversation_id: conversationID,
+            text: newMessageObj.text,
+            context:
+              messages.length >= 3
+                ? `user: ${messages[messages.length - 3].text} ` +
+                  `ai: ${messages[messages.length - 2].text}`
+                : "",
+            lat: userLocation.lat,
+            lng: userLocation.lng,
+          }),
+        });
+
+        let response_json = await response.json();
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          {
+            id: prevMessages.length,
+            sender: "chatbot",
+            text: response_json.ai_response.text,
+            timestamp: new Date(),
+          },
+        ]);
+      } catch (error) {
+        console.error("Failed to send message:", error);
+      }
     }
   };
 
